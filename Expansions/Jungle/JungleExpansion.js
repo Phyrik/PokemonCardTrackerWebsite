65dd2c9888fd8b0f5c8af303c3907b1413245444
Phyrik <34688504+Phyrik@users.noreply.github.com>
2018-08-21 17:27:12 +0100

    added case for bellsprout

--- Expansions/Jungle/JungleExpansion.js
+++ Expansions/Jungle/JungleExpansion.js
@@ -1,3 +1,9 @@
 function editPokemon(pokemon) {
-    
+    switch (pokemon) {
+        case bellsprout:
+            alert('Editing Bellsprout...');
+            break;
+        default:
+            alert('Sorry! We ran into a problem! Try again.');
+    }
 }
